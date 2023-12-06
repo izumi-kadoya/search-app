c86b953639a58ed3ecb7683b59bbd24edd96a5d3
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-12-06 10:12:21 +0900

    API

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,34 @@
 class SearchesController < ApplicationController
- 
+  require 'httparty'
+
   def index
   end
+
+
+
+  def create
+    # ユーザーからのキーワードを取得
+    keywords = [params[:keyword_1], params[:keyword_2], params[:keyword_3]].join(" ")
+
+    # OpenAI APIにリクエストを送信
+    response = HTTParty.post(
+      "https://api.openai.com/v1/engines/davinci-codex/completions",
+      headers: {
+        "Authorization" => "Bearer YOUR_API_KEY",
+        "Content-Type" => "application/json"
+      },
+      body: {
+        prompt: keywords,
+        max_tokens: 100
+        model: "gpt-4"
+      }.to_json
+    )
+
+    # レスポンスをビューに渡す
+    @response = response.parsed_response
+
+    render 'show'
+  end
+end
 
 end
