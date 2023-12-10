8af7b1b67c3e86e8fcd824ada06f2efb2b388802
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-12-10 10:55:27 +0900

    検索ボックスの追加

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -1,4 +1,25 @@
+require 'httparty'
+
 class ScrapsController < ApplicationController
   def index
+    if search_params_present?
+      api_key = ENV['GOOGLE_API_KEY']
+      cse_id = ENV['GOOGLE_CSE_ID']
+      keywords = [params[:keyword_1], params[:keyword_2], params[:keyword_3]].join(' ')
+
+      response = HTTParty.get("https://www.googleapis.com/customsearch/v1", query: {
+        key: api_key,
+        cx: cse_id,
+        q: keywords
+      })
+
+      @results = response.parsed_response if response.code == 200
+    end
+  end
+
+  private
+
+  def search_params_present?
+    params[:keyword_1].present? || params[:keyword_2].present? || params[:keyword_3].present?
   end
 end
